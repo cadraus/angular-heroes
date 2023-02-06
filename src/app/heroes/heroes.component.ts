a42008a54ca28f052e9b4565a074a687b5303205
Alex <cadraus@gmail.com>
2023-02-06 09:36:14 +0000

    2. Display a list and css buttons

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +10,12 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 
   constructor() { }
-  hero: Hero = {
-  id: 1,
-  name: 'DyatloStoock'
-  };
+  heroes = HEROES;
+  
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit(): void {
   }
