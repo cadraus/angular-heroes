cce34d71ac91f3ecc3a061429d3b8ec08a8f5b1f
Alex <cadraus@gmail.com>
2023-02-12 16:19:39 +0000

    4. Services and Messages added

---
+++ src/app/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+constructor(private messageService: MessageService) { }
+
+// For asynchronos calls, the observable in set in services. 
+// The methods of components subsrribes to this observble. 
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  
+}
+
+
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
 
-  constructor() { }
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
   
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[] = [];
 
-  ngOnInit(): void {
-  }
+  constructor(private heroService: HeroService, 
+    private messageService: MessageService) { }
+  
+    ngOnInit(): void {
+      this.getHeroes();
+    }
+
+    onSelect(hero: Hero): void {
+      
+      //current date and time
+      const current = new Date();
+      const currentDate = current.toDateString();
+      const currentTime = current.toLocaleTimeString();
+
+
+      this.selectedHero = hero;
+      this.messageService.add(
+        `HeroesComp.- ${hero.id}`+
+                  `, ${hero.name}`+`
+        , selected at: ` + currentDate + `, ` +  currentTime);
+    }
+  
+    getHeroes(): void {
+      this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    }
 
 }
